b7733e82bf3396722984cddc5a9eb1552128b63b
Ethan Cloin <psnethan@gmail.com>
2024-09-02 14:33:50 -0400

    Add color palette and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,55 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        domine: ["Domine", "serif"],
+      },
+      colors: {
+        fern: {
+          50: "#ecf4ea",
+          100: "#d1e3cb",
+          200: "#b5d1ab",
+          300: "#9ac08b",
+          400: "#86b374",
+          500: "#75a65e",
+          600: "#6b9855",
+          700: "#5e864b",
+          800: "#547543",
+          900: "#415635",
+        },
+        sage: {
+          50: "#eeffe7",
+          100: "#e9fde2",
+          200: "#e5f8de",
+          300: "#e0f4d9",
+          400: "#bfd2b8",
+          500: "#a2b59b",
+          600: "#788a72",
+          700: "#63755e",
+          800: "#44553f",
+          900: "#22321e",
+        },
+        sienna: {
+          50: "#fcedee",
+          100: "#f9d1d2",
+          200: "#e4a29b",
+          300: "#d78076",
+          400: "#de6954",
+          500: "#e05f3d",
+          600: "#d2573b",
+          700: "#c14e36",
+          800: "#b34830",
+          900: "#a53f25",
+        },
+        parchment: {
+          light: "#EFEFE7",
+          DEFAULT: "#DEDDCE",
+          dark: "#BFB99C",
+        },
+      },
+    },
   },
   plugins: [],
 };
